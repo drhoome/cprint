09fee27bda8e8c97c19a6675bc6ee154e76d4089
Artur Paiva <dr.hoome@gmail.com>
2015-12-12 15:45:52 -0200

    Switched version to 0.2 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name="mprint",
-        version="0.11",
+        version="0.2",
         description="Use tags to print markup text on console screen",
         classifiers=[
             "Development Status :: 4 - Beta",
